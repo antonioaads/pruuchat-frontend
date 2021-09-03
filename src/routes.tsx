3a0c043cb5a9e2e5f44beb35aba46ab453ec5e2e
Antonio Augusto Diniz Sousa <antonio.diniz.ext@dasa.com.br>
2021-09-02 23:53:45 -0300

    wip: initial testes with socket

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import routes from "./utils/routersDefinitions";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Teste from "./pages/Teste";
 
 const Routes: React.FC = () => (
   <Switch>
@@ -12,6 +13,7 @@ const Routes: React.FC = () => (
     <Route path={routes.home} exact component={Home} />
     <Route path={routes.login} exact component={Login} />
     <Route path={routes.register} exact component={Register} />
+    <Route path={'/teste'} exact component={Teste} />
   </Switch>
 );
 
